7b952cb81e107bf222838bd923463ecceb59d2fd
Nicolas Henin <mr.henin@gmail.com>
2020-07-29 16:45:05 +0200

    removed packcheck files added by mistake

--- .packcheck/dolla-consensus-base-1.0.0/lib/Dolla/Consensus/Voting/Estimate.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE DerivingVia, DeriveGeneric, FlexibleContexts,
-  UndecidableInstances, StandaloneDeriving #-}
-module Dolla.Consensus.Voting.Estimate (Estimate (..)) where
-
-import           Data.Aeson
-import           GHC.Generics
-
-newtype Estimate = Estimate Bool deriving (Eq,Show, Generic , ToJSON,FromJSON) via Bool
